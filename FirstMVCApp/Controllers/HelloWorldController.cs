e5ae3c20e73fcb03535b88f69ad79a8d59133644
Radif Elnoun <SIRIOSUKUR@GMAIL.COM>
2023-05-02 12:20:43 +0200

    Add project files.

---
+++ FirstMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,25 @@
+﻿using FirstMVCApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            DogViewModel doggo = new DogViewModel() 
+            { Name="Sif", Age=2};
+              return View(doggo);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public string Hello()
+        {
+            return "Whos's there?";
+        }
+    }
+}
